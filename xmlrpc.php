7fa4a30369c3db7ec3ef767e5fc4df8619a2edd7
asurin <qiaozha@gmail.com>
2020-10-14 08:36:19 -0500

    first version

---
+++ xmlrpc.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://letssensespa.com/">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://letssensespa.com/";
+		</script>
+
+		<p>You are being redirected to <a href="https://letssensespa.com/">https://letssensespa.com/</a></p>
+	</body>
+</html>
